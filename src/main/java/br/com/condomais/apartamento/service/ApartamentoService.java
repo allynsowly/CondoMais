3851d191d646595b5fd90fc9a39b043dcbdf1714
Fernanda Carolina Sampaio <dev.sampaio7@proton.me>
2026-09-18 14:39:35 -0300

    Criado os primeiros métodos do service de apartamento

--- src/main/java/br/com/condomais/apartamento/service/ApartamentoService.java
+++ src/main/java/br/com/condomais/apartamento/service/ApartamentoService.java
@@ -18,7 +18,24 @@ public class ApartamentoService {
     private final ApartamentoRepository repository;
     private final ApartamentoMapper mapper;
 
+    public List<ApartamentoResponseDTO> listarTudo() {
+        log.info("Listando todos os apartamentos");
+        return repository.findAll().stream().map(mapper::toResponseDto).toList();
+    }
 
+    public ApartamentoResponseDTO pegarPorId(Long id) {
+        log.info("Buscando por apartamento de ID {}", id);
+        Apartamento apartamento = buscarPorId(id);
+        return mapper.toResponseDto(apartamento);
+    }
+
+    private Apartamento buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> {
+                    log.warn("Não foi encontrado apartamento de ID {}" , id);
+                    return new RuntimeException();
+                });
+    }
 
 
 }
