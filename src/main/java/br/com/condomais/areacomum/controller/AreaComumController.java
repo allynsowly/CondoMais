3f890f2c93af20046dc9a09c0fd05b09b952e4d9
Fernanda Carolina Sampaio <dev.sampaio7@proton.me>
2026-09-18 15:48:41 -0300

    Criado o controller de area comum

--- src/main/java/br/com/condomais/areacomum/controller/AreaComumController.java
+++ src/main/java/br/com/condomais/areacomum/controller/AreaComumController.java
@@ -1,4 +1,48 @@
 package br.com.condomais.areacomum.controller;
 
+import br.com.condomais.areacomum.DTOs.AreaComumCreateDTO;
+import br.com.condomais.areacomum.DTOs.AreaComumResponseDTO;
+import br.com.condomais.areacomum.DTOs.AreaComumUpdateDTO;
+import br.com.condomais.areacomum.service.AreaComumService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/area-comum")
+@RequiredArgsConstructor
 public class AreaComumController {
+
+    private final AreaComumService service;
+
+    @GetMapping
+    public ResponseEntity<List<AreaComumResponseDTO>> listarTudo() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.listarTudo());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AreaComumResponseDTO> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<AreaComumResponseDTO> criar(@RequestBody @Valid AreaComumCreateDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.criar(dto));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<AreaComumResponseDTO> atualizar(@PathVariable Long id, @RequestBody @Valid AreaComumUpdateDTO dto) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.atualizar(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar( @PathVariable Long id) {
+        service.deletar(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 }
